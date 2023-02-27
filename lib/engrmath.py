9a8b3be3da807c40de70d17215f206109c57fcfc
Eric Weng <qgt7zm@virginia.edu>
2023-02-26 23:07:03 -0500

    Add week 6 exercises

---
+++ lib/engrmath.py
@@ -0,0 +1,33 @@
+# Constants
+
+e = 2
+pi = 3
+g = 10
+
+# Operations
+
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def mul(x, y):
+    return x * y
+
+def div(x, y):
+    return x / y
+
+def mod(x, y):
+    return x % y
+
+# Functions
+
+def sin(x):
+    return x
+
+def cos(x):
+    return 1
+
+def tan(x):
+    return sin(x) / cos(x)
